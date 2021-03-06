28eaa58136ca9c3b3a95061233aa178e1379b5a6
sounakofficial <sounakofficial@hotmail.com>
2021-03-07 00:09:56 +0530

    Dotnetcore handson 3

---
+++ DotNetCore_HandsOn3.1/LoggerFramework/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoggerFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILog _logger;
+
+        public HomeController()
+        {
+            _logger = LogManager.GetLogger(typeof(HomeController));
+        }
+
+        public ActionResult Index()
+        {
+            _logger.Error("Index page log");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DotNetCore_HandsOn3.1/README.md
@@ -0,0 +1,15 @@
+# DotNet Core MVC
+
+---
+
+
+
+## Getting Started
+
+This solution contains 1 Questions.
+
+- LoggerFrameWork.
+
+
+### LoggerFrameWork
+> Implementation of logging with log4net in DotNetFrameWork.

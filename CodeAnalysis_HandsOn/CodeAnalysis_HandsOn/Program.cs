c074cc6eea153c37bc613befbc09877d16857e89
sounakofficial <sounakofficial@hotmail.com>
2021-03-07 10:33:07 +0530

    codeanalysis

---
+++ CodeAnalysis_HandsOn/CodeAnalysis_HandsOn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeAnalysis_HandsOn
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int a = 10;
+        }
+    }
+}
---
+++ CodeAnalysis_HandsOn/README.md
@@ -0,0 +1,15 @@
+# CodeAnalysis HandsOn
+
+---
+
+
+
+## Getting Started
+
+This solution contains 1 Question.
+
+- CodeAnalysis_HandsOn.
+
+### CodeAnalysis_HandsOn
+
+> Observing results of code analysis.
